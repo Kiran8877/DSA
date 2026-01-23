ecf5e18cb12c41bdb7e34a6af988ab32ff608610
Kiran8877 <152734409+Kiran8877@users.noreply.github.com>
2026-01-23 11:09:22 +0530

    Time: 3 ms (46.13%) | Memory: 8.6 MB (39.39%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int num=x;
+        long long n=0;
+        while(x>0){
+            n=n*10+x%10;
+            x=x/10;
+        }
+        return num==n;
+        
+    }
+};
