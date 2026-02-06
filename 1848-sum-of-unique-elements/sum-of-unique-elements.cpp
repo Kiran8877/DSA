f2f297a5603d92fd8efb68653233ce4a615d83a8
Kiran Sai Karthik <152734409+Kiran8877@users.noreply.github.com>
2026-02-06 10:55:36 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (16.07%) - LeetSync

---
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        int sum=0;
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        for(auto it:mp){
+            if(it.second==1){
+                sum=sum+it.first;
+            }
+        }
+        return sum;
+    }
+};
